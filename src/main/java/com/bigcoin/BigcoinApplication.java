e2c829046ae20853f7daa103bc1a905929ea1644
songjaeho <wogh0935@naver.com>
2021-04-12 22:06:07 +0900

    [2021-04-12]

---
+++ src/main/java/com/bigcoin/BigcoinApplication.java
@@ -0,0 +1,13 @@
+package com.bigcoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigcoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigcoinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bigcoin/BigcoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigcoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigcoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
